2c9bc8a5bf1952d5b667c21e6c12349a2c7f8dab
Ahmed Diab <AhmedDiab22@gmail.com>
2019-05-15 05:26:52 +0200

    modify

--- js/resume.js
+++ js/resume.js
@@ -1,5 +1,4 @@
 $(function () {
-
     $('.collapse ul li a , i').click(function () {
         $('html , body').animate({
             scrollTop : $('#'+ $(this).data('value')).offset().top
@@ -9,4 +8,30 @@ $(function () {
     $('.collapse ul li a').click(function () {
         $(this).addClass("active").siblings().removeClass("active");
       })
+
+      
  });
+
+ $("html").niceScroll(({
+     cursorcolor:"#039be5 ",
+     cursorwidth : '10px',
+     
+}));
+
+
+
+$('#container').isotope({
+    itemSelector : '.item',
+    layoutMode : 'fitRows'
+});
+$('.sec__content ul li').click(function () {
+    $('.sec__content ul li').removeClass('selected');
+    $(this).addClass('selected');
+
+    var selector = $(this).attr("data-filter");
+
+    $('#container').isotope({
+        filter : selector
+    });
+    return false
+  })
